d58a814860512f3f167921c92be04cf64142d069
hnrqblck <hnrqblck@gmail.com>
2022-06-21 13:41:30 -0300

    [JS-242] create journey's link

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,7 +10,7 @@ import {
   Tr,
   Button,
   Flex,
-  Text,
+  Link,
 } from "@chakra-ui/react";
 import { useEffect, useState, Fragment } from "react";
 import * as api from "strateegia-api";
@@ -18,7 +18,6 @@ import Loading from "../components/Loading";
 import MapList from "../components/MapList";
 import ProjectList from "../components/ProjectList";
 import { extractUserCommentInfo } from "../data/graphData";
-import * as d3 from "d3";
 import { exportTableAsCsv, exportJson } from "../utils/exportFunctions";
 import { i18n } from "../translate/i18n";
 
@@ -30,9 +29,21 @@ export default function Main() {
   const [accessToken, setAccessToken] = useState("");
   const [commentsReport, setCommentsReport] = useState(null);
   const [rawData, setRawData] = useState(null);
+  const [projectData, setProjectData] = useState(null);
 
   const handleSelectChange = (e) => {
     setSelectedProject(e.target.value);
+    setIsLoading(true);
+    async function fetchProjectData() {
+      try {
+        const project = await api.getProjectById(accessToken, e.target.value);
+        setProjectData(project);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProjectData();
   };
 
   const handleMapSelectChange = (e) => {
@@ -107,7 +118,24 @@ export default function Main() {
       <Heading as="h3" size="md" mb={3}>
         {i18n.t('main.heading')}
       </Heading>
-      <ProjectList handleSelectChange={handleSelectChange} />
+      <Box display='flex' >
+        <ProjectList handleSelectChange={handleSelectChange} />
+        <Link 
+          pointerEvents={selectedProject ? '' : 'none'}
+          _disabled={selectedProject ? false : true}
+          href={selectedProject?.length > 0 ? `https://app.strateegia.digital/journey/${selectedProject}/map/${projectData?.maps[0].id}` : '' }
+          target='_blank'
+          bg='#E9ECEF'
+          borderRadius={' 0 6px 6px 0 '}
+          fontSize={16}
+          w={200} h='40px'
+          display='flex'
+          alignItems='center'
+          justifyContent='center'
+        >
+          {i18n.t('main.link')}
+        </Link>
+      </Box>
       {/* <MapList
         projectId={selectedProject}
         handleSelectChange={handleMapSelectChange}
